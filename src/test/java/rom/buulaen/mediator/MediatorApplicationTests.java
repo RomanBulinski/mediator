96d5908cff4d373fe81a21907064534b04fc5638
bulir <aboandrus@gamil.com>
2024-10-02 17:47:27 +0200

    start application

---
+++ src/test/java/rom/buulaen/mediator/MediatorApplicationTests.java
@@ -0,0 +1,13 @@
+package rom.buulaen.mediator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
